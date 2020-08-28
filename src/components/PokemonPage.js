5c6a832bf8d748bab9752e887aea0efa7ff71f8d
sarahmr63089 <sarahrose63089@gmail.com>
2020-08-28 15:10:44 -0400

    Done.

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,53 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    searchTerm: '',
+    pokemon: []
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/pokemon')
+    .then(res => res.json())
+    .then((pokemonObjArr) => {
+      this.setState({
+        pokemon: pokemonObjArr
+      })
+    })
+  }
+
+  handleSearch = (searchTerm) => {
+    this.setState({ searchTerm })
+  }
+
+  handleFormOutput = (pokemon) => {
+    fetch("http://localhost:3000/pokemon", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(pokemon)
+    })
+    .then(res => res.json())
+    .then((pokeObj) => {
+      let newList = [...this.state.pokemon, pokeObj]
+      this.setState({
+        pokemon: newList
+      })
+    })
+  }
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm handleFormOutput={this.handleFormOutput}/>
         <br />
-        <Search />
+        <Search search={this.handleSearch}/>
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.state.pokemon} search={this.state.searchTerm} />
       </Container>
     )
   }
