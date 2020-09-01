0bf16ffdec9699c9f9e3e3fd6b8981fb3a68ef3e
Valkryn <calvinraphaelg@gmail.com>
2020-09-01 13:09:24 -0400

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,24 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+
+  searchFilter = () => {
+    let pokemonSearchName = this.props.pokemonSearch
+    if (pokemonSearchName) {
+      return this.props.pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(pokemonSearchName.toLowerCase()))
+    } else {
+      return this.props.pokemons
+    }
+  }
+
+  renderPokemonCard = () => {
+    return this.searchFilter().map(pokemon => <PokemonCard key={pokemon.id} pokemon={pokemon} />)
+  }
+
   render() {
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        {this.renderPokemonCard()}
       </Card.Group>
     )
   }
