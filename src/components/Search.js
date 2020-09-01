85291713229104cb95e09b8d54ba4701eaeb1fce
21shield <21netaly@gmail.com>
2020-09-01 03:06:55 -0400

    Done.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Search = props => {
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt"/>
+        <input onChange={e => props.handleSearch(e.target.value)} className="prompt" />
         <i className="search icon" />
       </div>
     </div>
