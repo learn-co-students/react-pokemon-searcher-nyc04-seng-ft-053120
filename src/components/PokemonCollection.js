6ac89508f4decbe555fbd8b5f77aef99bfe0254d
Zeynab-j <zeina2025@gmail.com>
2020-09-01 20:21:47 -0400

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,14 +2,21 @@ import React from 'react'
 import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
+// will be iterating through the collection of information we have and rendering out a singular pokemon card for each
+
 class PokemonCollection extends React.Component {
   render() {
+    let arrPokemons = this.props.pokemons.map(eachPoke => {
+      return <PokemonCard key={eachPoke.name} pokemon={eachPoke}/>
+    })
+
     return (
       <Card.Group itemsPerRow={6}>
         <h1>Hello From Pokemon Collection</h1>
+        {arrPokemons}
       </Card.Group>
     )
   }
 }
 
-export default PokemonCollection
+export default PokemonCollection;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const Search = props => {
+  let changeHandle = (evt) => {
+    props.changeSearch(evt.target.value)
+
+  }
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt"/>
+        <input className="prompt" value={props.searchWord} onChange={changeHandle}/>
         <i className="search icon" />
       </div>
     </div>
